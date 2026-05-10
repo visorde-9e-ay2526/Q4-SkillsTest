f06b55abb97a9712811fd1e9a0e3d3dbc1263c07
visorde-9e-ay2526 <visorde.cm@obmontessori.edu.ph>
2026-05-11 07:51:18 +0800

    update login, notebooks, and js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+/*function to handle the login process*/
 function logIn(event) {
+    /*ensure that the form does not submit and refresh the page*/
     event.preventDefault();
+    /*values of the username and password input fields*/
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
+    /*method to check the username and password against predefined values*/
     switch (username) {
         case "admin":
             if (password === "admin123") {
